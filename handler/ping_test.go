04714b3209256a1be49d386a51b004af1cf62bca
cellargalaxy <cellargalaxy@gmail.com>
2026-09-13 23:20:19 +0800

    1

--- handler/ping_test.go
+++ handler/ping_test.go
@@ -1,12 +1,10 @@
 package handler_test
 
 import (
-	"bytes"
 	"encoding/base64"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
 	"time"
@@ -14,93 +12,19 @@ import (
 	common_model "github.com/cellargalaxy/go_common/model"
 	"github.com/cellargalaxy/go_common/util"
 	"github.com/cellargalaxy/jotcash/config"
-	"github.com/cellargalaxy/jotcash/db"
-	"github.com/cellargalaxy/jotcash/handler"
-	"github.com/cellargalaxy/jotcash/tool"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
-func TestMain(m *testing.M) {
-	gin.SetMode(gin.TestMode)
-	logrus.SetLevel(logrus.WarnLevel)
-	code := m.Run()
-	//db包的init会在测试二进制的原始工作目录建库，不清掉会污染代码目录
-	os.RemoveAll("resource")
-	os.RemoveAll("log")
-	os.Exit(code)
-}
-
-func catchLog(t *testing.T) *bytes.Buffer {
-	t.Helper()
-	buffer := new(bytes.Buffer)
-	origin := logrus.StandardLogger().Out
-	logrus.SetOutput(buffer)
-	t.Cleanup(func() { logrus.SetOutput(origin) })
-	return buffer
-}
-
-func findLogField(text, key string) string {
-	index := strings.Index(text, "["+key+":")
-	if index < 0 {
-		return ""
-	}
-	text = text[index+len(key)+2:]
-	index = strings.Index(text, "]")
-	if index < 0 {
-		return ""
-	}
-	return text[:index]
-}
-
-// 初始前端口令只在建库那一次进日志，测试从日志里捞
-func newTestEngine(t *testing.T) (*gin.Engine, string) {
-	t.Helper()
-	t.Chdir(t.TempDir())
-
-	ctx := util.GenCtx()
-	buffer := catchLog(t)
-	err := db.Create(ctx)
-	if err != nil {
-		t.Fatalf("建测试库异常: %+v", err)
-	}
-	clientToken := findLogField(buffer.String(), "clientToken")
-	if clientToken == "" {
-		t.Fatalf("没捞到初始前端口令: %s", buffer.String())
-	}
-	return handler.NewEngine(ctx), clientToken
-}
-
-func newJwt(t *testing.T, serverToken, clientToken string, expire time.Duration) string {
-	t.Helper()
-	jwt, err := tool.EnJwt(util.GenCtx(), serverToken, clientToken, expire)
-	if err != nil {
-		t.Fatalf("签发jwt异常: %+v", err)
-	}
-	return jwt
-}
-
 func postPing(t *testing.T, engine *gin.Engine, request *http.Request) common_model.HttpResp {
 	t.Helper()
-	writer := httptest.NewRecorder()
-	engine.ServeHTTP(writer, request)
-	if writer.Code != http.StatusOK {
-		t.Fatalf("http状态码不符: got=%d want=%d", writer.Code, http.StatusOK)
-	}
 	var resp common_model.HttpResp
-	err := util.JsonStr2Struct(writer.Body.String(), &resp)
-	if err != nil {
-		t.Fatalf("响应体解析异常: body=%s err=%+v", writer.Body.String(), err)
-	}
+	doRequest(t, engine, request, &resp)
 	return resp
 }
 
 func newPingRequest(jwt string) *http.Request {
-	request := httptest.NewRequest(http.MethodPost, util.PathPing, nil)
-	if jwt != "" {
-		request.Header.Set(util.AuthorizationKey, util.BearerKey+" "+jwt)
-	}
-	return request
+	return newRequest(util.PathPing, jwt, nil)
 }
 
 func TestPing(t *testing.T) {
