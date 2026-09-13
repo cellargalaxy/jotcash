19538dbf9ba1d42246e9f40540e6ba6b1f650e3f
cellargalaxy <cellargalaxy@gmail.com>
2026-09-13 13:26:54 +0800

    1

---
+++ model/config.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"github.com/cellargalaxy/go_common/util"
+)
+
+type Config struct {
+	DbPath      string `json:"db_path" yaml:"db_path"`
+	ServerToken string `json:"-" yaml:"db_path"` //后端口令
+}
+
+func (this Config) String() string {
+	return util.JsonStruct2Str(this)
+}
