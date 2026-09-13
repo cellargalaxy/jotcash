58b093eae4649884b0749ad13dd3b4849086b804
cellargalaxy <cellargalaxy@gmail.com>
2026-09-13 21:54:06 +0800

    1

--- exchange_rate/exchange_rate.go
+++ exchange_rate/exchange_rate.go
@@ -2,11 +2,12 @@ package exchange_rate
 
 import (
 	"context"
+	"time"
 
 	"github.com/shopspring/decimal"
 )
 
-func GetExchangeRate(ctx context.Context, expenseCurrency, accountingCurrency string) (decimal.Decimal, error) {
+func GetExchangeRate(ctx context.Context, expenseDate time.Time, expenseCurrency, accountingCurrency string) (decimal.Decimal, error) {
 	//todo
 	//具体实现先挂起，代码写死返回1
 	return decimal.NewFromInt(1), nil
