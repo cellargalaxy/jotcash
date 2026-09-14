f0184b88f0838112f44fb1f39896a869b0e7dbfd
cellargalaxy <cellargalaxy@gmail.com>
2026-09-14 11:46:54 +0800

    1

---
+++ handler/expense.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/cellargalaxy/go_common/util"
+	"github.com/cellargalaxy/jotcash/model"
+	"github.com/cellargalaxy/jotcash/service"
+	"github.com/cellargalaxy/jotcash/tool"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func InsertExpense(c *gin.Context) {
+	logrus.WithContext(c).WithFields(logrus.Fields{"claims": tool.GetClaims(c)}).Info("明细入库")
+	fileHeader, err := c.FormFile(model.ExpenseFileKey)
+	if err != nil {
+		logrus.WithContext(c).WithFields(logrus.Fields{"err": err}).Error("明细入库，取上传文件异常")
+		c.JSON(http.StatusOK, util.NewHttpRespByErr(nil, errors.Errorf("明细入库，取上传文件异常: %+v", err)))
+		return
+	}
+	file, err := fileHeader.Open()
+	if err != nil {
+		logrus.WithContext(c).WithFields(logrus.Fields{"err": err}).Error("明细入库，打开上传文件异常")
+		c.JSON(http.StatusOK, util.NewHttpRespByErr(nil, errors.Errorf("明细入库，打开上传文件异常: %+v", err)))
+		return
+	}
+	defer util.CloseIo(c, file)
+	c.JSON(http.StatusOK, util.NewHttpRespByErr(service.InsertExpense(c, fileHeader.Filename, file)))
+}
