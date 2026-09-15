7c8ad6359e6602401069a1d7ad32d2ba0d3718b7
cellargalaxy <cellargalaxy@gmail.com>
2026-09-15 20:42:30 +0800

    1

--- service/repo/file_meta.go
+++ service/repo/file_meta.go
@@ -29,6 +29,34 @@ func SelectFileMeta(ctx context.Context, inquiry model.FileMetaInquiry) ([]*mode
 	return fileMetaHandler.Object, fileMetaHandler.Count, nil
 }
 
+func DownloadFile(ctx context.Context, id int64) (*model.FileMeta, []byte, error) {
+	transaction, err := rdb.NewTransaction(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer transaction.Close(ctx)
+
+	downloadHandler := rdb.NewFileDownloadHandler(id)
+	err = transaction.AddCommit(downloadHandler).Exec(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	if downloadHandler.FileMeta == nil {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"id": id}).Warn("文件下载，文件不存在")
+		return nil, nil, errors.Errorf("文件下载，文件不存在: %d", id)
+	}
+	if downloadHandler.FileBlob == nil {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"id": id, "fileHash": downloadHandler.FileMeta.FileHash}).Error("文件下载，文件内容缺失")
+		return nil, nil, errors.Errorf("文件下载，文件内容缺失: %d", id)
+	}
+	fileHash := util.EnSha256Hex(string(downloadHandler.FileBlob.FileData))
+	if fileHash != downloadHandler.FileBlob.FileHash {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"id": id, "fileHash": fileHash, "want": downloadHandler.FileBlob.FileHash}).Error("文件下载，文件数据已损坏")
+		return nil, nil, errors.Errorf("文件下载，文件数据已损坏: %d", id)
+	}
+	return downloadHandler.FileMeta, downloadHandler.FileBlob.FileData, nil
+}
+
 func checkFileMetaInquiry(ctx context.Context, inquiry model.FileMetaInquiry) (model.FileMetaInquiry, error) {
 	err := checkTimeRange(ctx, inquiry.CreatedAtStart, inquiry.CreatedAtEnd)
 	if err != nil {
