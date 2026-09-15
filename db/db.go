e7e9c1b1a4103b537b53f9a27414d15ef6cf9e40
cellargalaxy <cellargalaxy@gmail.com>
2026-09-15 13:02:01 +0800

    1

--- db/db.go
+++ db/db.go
@@ -24,93 +24,21 @@ const vfsName = "adiantum"
 
 var dbLock sync.RWMutex
 
-func connect(ctx context.Context, dbPath, token string) (*gorm.DB, error) {
-	if token == "" {
-		logrus.WithContext(ctx).WithFields(logrus.Fields{}).Error("连接数据库，口令为空")
-		return nil, errors.Errorf("连接数据库，口令为空")
-	}
-	folderPath, _ := path.Split(dbPath)
-	if folderPath != "" {
-		err := util.CreateFolderPath(ctx, folderPath)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	sqlDb, err := driver.Open(fmt.Sprintf("file:%s?vfs=%s&_txlock=immediate", dbPath, vfsName), func(conn *sqlite3.Conn) error {
-		err := conn.Exec(fmt.Sprintf("PRAGMA textkey=%s;", sqlite3.Quote(token)))
-		if err != nil {
-			return err
-		}
-		return conn.Exec("PRAGMA temp_store=memory;PRAGMA busy_timeout=10000;")
-	})
+func init() {
+	ctx := util.GenCtx()
+	err := Create(ctx)
 	if err != nil {
-		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath, "err": err}).Error("连接数据库，异常")
-		return nil, errors.Errorf("连接数据库，异常: %+v", err)
+		panic(err)
 	}
-
-	var version int
-	err = sqlDb.QueryRowContext(ctx, "PRAGMA user_version").Scan(&version)
-	if err != nil {
-		util.CloseIo(ctx, sqlDb)
-		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath, "err": err}).Error("连接数据库，口令错误或数据库文件损坏")
-		return nil, errors.Errorf("连接数据库，口令错误或数据库文件损坏")
-	}
-
-	gormDb, err := gorm.Open(gormlite.OpenDB(sqlDb), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{SingularTable: true},
-		Logger:         util.NewDefaultGormLog(),
-	})
-	if err != nil {
-		util.CloseIo(ctx, sqlDb)
-		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath, "err": err}).Error("连接数据库，gorm初始化异常")
-		return nil, errors.Errorf("连接数据库，gorm初始化异常: %+v", err)
-	}
-	return gormDb, nil
 }
-func open(ctx context.Context, dbPath, token string) (*gorm.DB, error) {
-	if util.GetFileInfo(ctx, dbPath) == nil {
-		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath}).Error("打开数据库，库文件不存在")
-		return nil, errors.Errorf("打开数据库，库文件不存在")
-	}
-	db, err := connect(ctx, dbPath, token)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-func Open(ctx context.Context) (*gorm.DB, error) {
+
+func Create(ctx context.Context) error {
 	dbPath := config.DbPath
-	token, err := tool.GetToken(ctx)
+	token, err := tool.GenToken(ctx, tool.TokenLen)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 
-	dbLock.RLock()
-	defer dbLock.RUnlock()
-
-	db, err := open(ctx, dbPath, token)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-func CheckToken(ctx context.Context) error {
-	db, err := Open(ctx)
-	if err != nil {
-		return err
-	}
-	return util.CloseDb(ctx, db)
-}
-func checkToken(ctx context.Context, dbPath, token string) error {
-	db, err := open(ctx, dbPath, token)
-	if err != nil {
-		return err
-	}
-	return util.CloseDb(ctx, db)
-}
-
-func create(ctx context.Context, dbPath, token string) error {
 	info := util.GetFileInfo(ctx, dbPath)
 	if info != nil && info.Size() > 0 {
 		return nil
@@ -123,6 +51,30 @@ func create(ctx context.Context, dbPath, token string) error {
 	}
 	logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath}).Info("创建数据库")
 
+	err = create(ctx, dbPath, token)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func create(ctx context.Context, dbPath, token string) error {
+	info := util.GetFileInfo(ctx, dbPath)
+	if info != nil && info.Size() > 0 {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath}).Error("创建数据库，库文件已存在")
+		return errors.Errorf("创建数据库，库文件已存在")
+	}
+	if token == "" {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{}).Error("创建数据库，口令为空")
+		return errors.Errorf("创建数据库，口令为空")
+	}
+
+	folderPath, _ := path.Split(dbPath)
+	if folderPath != "" {
+		err := util.CreateFolderPath(ctx, folderPath)
+		if err != nil {
+			return err
+		}
+	}
 	err := util.WriteData2File(ctx, nil, dbPath)
 	if err != nil {
 		return err
@@ -169,21 +121,93 @@ func create(ctx context.Context, dbPath, token string) error {
 	}).Info("创建数据库，口令")
 	return nil
 }
-func NewTransaction(ctx context.Context) (*util.Transaction, error) {
+
+func Open(ctx context.Context) (*gorm.DB, error) {
 	dbPath := config.DbPath
 	token, err := tool.GetToken(ctx)
 	if err != nil {
 		return nil, err
 	}
-	err = create(ctx, dbPath, token)
+
+	dbLock.RLock()
+	defer dbLock.RUnlock()
+
+	db, err := open(ctx, dbPath, token)
 	if err != nil {
 		return nil, err
 	}
+	return db, nil
+}
+func open(ctx context.Context, dbPath, token string) (*gorm.DB, error) {
+	info := util.GetFileInfo(ctx, dbPath)
+	if info == nil || info.Size() <= 0 {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath}).Error("打开数据库，库文件不存在")
+		return nil, errors.Errorf("打开数据库，库文件不存在")
+	}
+	if token == "" {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{}).Error("连接数据库，口令为空")
+		return nil, errors.Errorf("连接数据库，口令为空")
+	}
+
+	sqlDb, err := driver.Open(fmt.Sprintf("file:%s?vfs=%s&_txlock=immediate", dbPath, vfsName), func(conn *sqlite3.Conn) error {
+		err := conn.Exec(fmt.Sprintf("PRAGMA textkey=%s;", sqlite3.Quote(token)))
+		if err != nil {
+			return err
+		}
+		return conn.Exec("PRAGMA temp_store=memory;PRAGMA busy_timeout=10000;")
+	})
+	if err != nil {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath, "err": err}).Error("连接数据库，异常")
+		return nil, errors.Errorf("连接数据库，异常: %+v", err)
+	}
+
+	var version int
+	err = sqlDb.QueryRowContext(ctx, "PRAGMA user_version").Scan(&version)
+	if err != nil {
+		util.CloseIo(ctx, sqlDb)
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath, "err": err}).Error("连接数据库，口令错误或数据库文件损坏")
+		return nil, errors.Errorf("连接数据库，口令错误或数据库文件损坏")
+	}
+
+	db, err := gorm.Open(gormlite.OpenDB(sqlDb), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{SingularTable: true},
+		Logger:         util.NewDefaultGormLog(),
+	})
+	if err != nil {
+		util.CloseIo(ctx, sqlDb)
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"dbPath": dbPath, "err": err}).Error("连接数据库，gorm初始化异常")
+		return nil, errors.Errorf("连接数据库，gorm初始化异常: %+v", err)
+	}
+	return db, nil
+}
+func CheckToken(ctx context.Context) error {
+	db, err := Open(ctx)
+	if err != nil {
+		return err
+	}
+	return util.CloseDb(ctx, db)
+}
+func checkToken(ctx context.Context, dbPath, token string) error {
+	db, err := open(ctx, dbPath, token)
+	if err != nil {
+		return err
+	}
+	return util.CloseDb(ctx, db)
+}
+
+func NewTransaction(ctx context.Context) (*DbTransaction, error) {
+	dbPath := config.DbPath
+	token, err := tool.GetToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+	dbLock.RLock()
 	object, err := newTransaction(ctx, dbPath, token)
 	if err != nil {
+		dbLock.RUnlock()
 		return nil, err
 	}
-	return object, nil
+	return &DbTransaction{Transaction: object}, nil
 }
 func newTransaction(ctx context.Context, dbPath, token string) (*util.Transaction, error) {
 	db, err := open(ctx, dbPath, token)
@@ -192,4 +216,13 @@ func newTransaction(ctx context.Context, dbPath, token string) (*util.Transactio
 	}
 	object := util.NewTransaction(db)
 	return object, nil
+}
+
+type DbTransaction struct {
+	*util.Transaction
+}
+
+func (this *DbTransaction) Close(ctx context.Context) error {
+	defer dbLock.RUnlock()
+	return this.Transaction.Close(ctx)
 }
