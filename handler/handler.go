a8dbe02f98f8bc0096297b8257adbcb2f38649e3
cellargalaxy <cellargalaxy@gmail.com>
2026-09-15 21:36:16 +0800

    1

--- handler/handler.go
+++ handler/handler.go
@@ -35,6 +35,7 @@ func NewEngine(ctx context.Context) *gin.Engine {
 	engine.POST(config.PathExpenseSelect, validate, util.NewGinPost("明细查询", service.SelectExpense))
 	engine.POST(config.PathExpenseUpdate, validate, util.NewGinPost("明细编辑", service.UpdateExpense))
 	engine.POST(config.PathExpenseDelete, validate, util.NewGinPost("明细删除", service.DeleteExpense))
+	engine.POST(config.PathExpenseSwitch, validate, util.NewGinPost("记账币种切换", service.SwitchAccountingCurrency))
 	engine.POST(config.PathExpenseDistinct, validate, util.NewGinPost("候选取值", service.SelectExpenseDistinct))
 	engine.POST(config.PathOperationLogSelect, validate, util.NewGinPost("审计查看", service.SelectOperationLog))
 	engine.POST(config.PathFileMetaSelect, validate, util.NewGinPost("文件列表", service.SelectFileMeta))
