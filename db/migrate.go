50a661a1e4ddb250ad6a4e2da0311ac188ebd81a
cellargalaxy <cellargalaxy@gmail.com>
2026-09-15 13:26:34 +0800

    1

--- db/migrate.go
+++ db/migrate.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cellargalaxy/jotcash/model"
 	"github.com/pkg/errors"
@@ -17,6 +18,16 @@ var migrateModels = []schema.Tabler{
 	&model.FileBlob{},
 }
 
+var migrateTriggers = []struct {
+	Table  string //挂触发器的表
+	Reason string //RAISE(ABORT)抛出去的原因，会原样出现在报错里
+}{
+	{Table: model.Expense{}.TableName(), Reason: "明细只能软删除，禁止物理删除"},
+	{Table: model.OperationLog{}.TableName(), Reason: "审计不可删除，禁止物理删除"},
+	{Table: model.FileMeta{}.TableName(), Reason: "文件不可删除，禁止物理删除"},
+	{Table: model.FileBlob{}.TableName(), Reason: "文件不可删除，禁止物理删除"},
+}
+
 func NewMigrateHandler() *MigrateHandler {
 	handler := new(MigrateHandler)
 	return handler
@@ -26,10 +37,10 @@ type MigrateHandler struct {
 }
 
 func (this *MigrateHandler) Exec(ctx context.Context, tx *gorm.DB) error {
-	if tx == nil {
-		logrus.WithContext(ctx).WithFields(logrus.Fields{}).Error("自动建表，连接为空")
-		return errors.Errorf("自动建表，连接为空")
-	}
+	return migrate(ctx, tx)
+}
+
+func migrate(ctx context.Context, tx *gorm.DB) error {
 	models := make([]any, 0, len(migrateModels))
 	for i := range migrateModels {
 		models = append(models, migrateModels[i])
@@ -39,6 +50,17 @@ func (this *MigrateHandler) Exec(ctx context.Context, tx *gorm.DB) error {
 		logrus.WithContext(ctx).WithFields(logrus.Fields{"err": err}).Error("自动建表，异常")
 		return errors.Errorf("自动建表，异常: %+v", err)
 	}
+
+	for i := range migrateTriggers {
+		table, reason := migrateTriggers[i].Table, migrateTriggers[i].Reason
+		sql := fmt.Sprintf("CREATE TRIGGER IF NOT EXISTS `%s_forbid_delete` BEFORE DELETE ON `%s` BEGIN SELECT RAISE(ABORT, '%s'); END;", table, table, reason)
+		err := tx.WithContext(ctx).Exec(sql).Error
+		if err != nil {
+			logrus.WithContext(ctx).WithFields(logrus.Fields{"table": table, "err": err}).Error("建防删触发器，异常")
+			return errors.Errorf("建防删触发器，异常: %+v", err)
+		}
+	}
+
 	logrus.WithContext(ctx).WithFields(logrus.Fields{}).Info("自动建表，完成")
 	return nil
 }
